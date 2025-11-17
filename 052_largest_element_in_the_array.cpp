690abb09ebb67a0034c768d3129cb25a605ae605
Meheru1115 <muhtashimaislam@gmail.com>
2025-11-17 15:39:05 +0600

    052_largest_elemet_in_the_array

---
+++ 052_largest_element_in_the_array.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 25, 3, 99, 45};
+    int n = 5;
+
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+
+    cout << "Largest element = " << largest;
+
+    return 0;
+}
