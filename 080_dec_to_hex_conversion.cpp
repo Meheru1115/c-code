8e301275483b2d40507a7d218bb68c32d841d981
Meheru1115 <muhtashimaislam@gmail.com>
2025-11-22 13:54:44 +0600

    080_dec_to_hex_conversion

---
+++ 080_dec_to_hex_conversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int dec;
+    cout << "Enter Decimal Number: ";
+    cin >> dec;
+
+    cout << "Hexadecimal: " << hex << dec << endl;
+
+    return 0;
+}
